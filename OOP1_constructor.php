71743690bf67c58992ed4ff394720a4b5beb431f
vasiliy1999 <vkolyasev1999@mail.ru>
2020-05-07 02:41:41 +0300

    new class OOP1

--- OOP1_constructor.php
+++ OOP1_constructor.php
@@ -1,5 +1,5 @@
 <?php
-class Article
+abstract class Article
 {
     public $title;
     public $titleFontSize;
@@ -9,8 +9,9 @@ class Article
     public $bg;
 
 //Это конструктор
-    public function __construct($title, $articleBody, $border, $bg,
-                                $titleFontSize = 14, $articleBodyFontSize = 20)
+// добавляем тип данных около каждого свойства
+    public function __construct(string $title, string $articleBody, string$border, string$bg,
+                                int $titleFontSize = 24, int $articleBodyFontSize = 14)
     {
         $this->title = $title;
         $this->titleFontSize = $titleFontSize;
@@ -31,9 +32,71 @@ class Article
 
 }
 
-$test2 = new Article('Заголовок', 'lorem222',
+class SportsArticle extends Article
+{
+    public $image;
+    public function __construct(string $title, string $articleBody, string $border, string $bg,
+                                string $image, int $titleFontSize = 24, int $articleBodyFontSize = 14)
+    {
+        $this->image = $image;
+        parent::__construct($title, $articleBody, $border, $bg, $titleFontSize, $articleBodyFontSize);
+    }
+    public function printArticle()
+    {
+        echo "<div style='border: {$this->border}; font-size: {$this->articleBodyFontSize}px; 
+                 background: {$this->bg}'><h2 style='font-size: {$this->titleFontSize}px'>
+                {$this->title}</h2><span>{$this->articleBody}</span>
+                <img src='{$this->image}' alt='Картинка' width='100'>
+              </div>";
+    }
+}
+//сейчас делаем разные виды статей, втавляем разные вещи туда
+class WeatherArticle extends Article
+{
+    public $temperature;
+    public function __construct(string $title, string $articleBody, string $border, string $bg, $temperature, int $titleFontSize = 24, int $articleBodyFontSize = 14)
+    {
+        $this->temperature = $temperature;
+        parent::__construct($title, $articleBody, $border, $bg, $titleFontSize, $articleBodyFontSize);
+    }
+    public function printArticle() {
+        echo "<div style='border: {$this->border}; font-size: {$this->articleBodyFontSize}px; 
+                 background: {$this->bg}'><h2 style='font-size: {$this->titleFontSize}px'>
+                {$this->title}</h2><span>{$this->articleBody}</span>
+                <span>{$this->temperature}<span/>
+              </div>";
+    }
+}
+class PolitikArticle extends Article
+{
+    public $link;
+    public function __construct(string $title, string $articleBody, string $border, string $bg, string $link, int $titleFontSize = 24, int $articleBodyFontSize = 14)
+    {
+        $this->link = $link;
+        parent::__construct($title, $articleBody, $border, $bg, $titleFontSize, $articleBodyFontSize);
+    }
+    public function printArticle() {
+        echo "<div style='border: {$this->border}; font-size: {$this->articleBodyFontSize}px; 
+                 background: {$this->bg}'><h2 style='font-size: {$this->titleFontSize}px'>
+                {$this->title}</h2><span>{$this->articleBody}</span>
+                <a href='{$this->link}'>{$this->link}</a>
+              </div>";
+    }
+}
+
+$sportsNews = new SportsArticle('Заголовок статьи про спорт', 'Текст...',
+    '3px solid red', 'whight', 'https://www.interfax.ru/ftproot/textphotos/2013/05/13/ga.jpg');
+$sportsNews->printArticle();
+$weatherNews = new WeatherArticle('Заголовок статьи про погоду', 'Текст...',
+    '10px solid red', 'green', '+20');
+$weatherNews->printArticle();
+$politiksNews = new PolitikArticle('Заголовок статьи про политику и пидоров', 'Текст...',
+    '3px solid red', 'whight', 'https:google.com');
+$politiksNews->printArticle();
+
+/*$test2 = new Article('Заголовок', 'lorem222',
     '2px solid green', 'blue');
 $test2->printArticle();
 $test = new Article('Спортивная новость', 'апрол',
     '2px solid green', 'green');
-$test->printArticle();
+$test->printArticle();*/
