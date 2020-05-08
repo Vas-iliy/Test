3172b3058e80191ffc0d4da853dd6596682d0be7
vasiliy1999 <vkolyasev1999@mail.ru>
2020-05-08 07:30:37 +0300

    PZ BD

---
+++ PZbD/data.php
@@ -0,0 +1,93 @@
+<?php
+
+return [
+  'about' => [
+      'name' => 'Колясев Василий',
+      'post' => 'php-разработчик',
+      'e-mail' => 'vkolyasev1999@mail.ru',
+      'phone' => '899998352445',
+      'site' => 'https://vk.com/kolyasev99'
+  ],
+    'education' => [
+        [
+            'faculty' => 'Random faculty',
+            'univercity' => 'Smth univercity',
+            'yearStart' => 2015,
+            'yearRnd' => 2019
+        ],
+        [
+            'faculty' => 'Random faculty2',
+            'univercity' => 'Smth univercity2',
+            'yearStart' => 2010,
+            'yearRnd' => 2015
+        ]
+    ],
+    'languages' => [
+        'English', 'Russian', 'Spainish'
+    ],
+    'interes' => [
+        'увлечение1', 'увлечение2'
+    ],
+    'aboutCareer' => 'lorem lorem lorem',
+
+    'career' => [
+        [
+            'post' => 'post 1',
+            'place' => 'place 1',
+            'duty' => 'lorem lorem 111',
+            'yearStart' => 2016,
+            'yearRnd' => 'настоящее время'
+
+        ],
+        [
+            'post' => 'post 2',
+            'place' => 'place 2',
+            'duty' => 'lorem lorem 222',
+            'yearStart' => 2012,
+            'yearRnd' => 2016
+        ],
+        [
+            'post' => 'post 3',
+            'place' => 'place 3',
+            'duty' => 'lorem lorem 333',
+            'yearStart' => 2010,
+            'yearRnd' => 2012
+        ]
+    ],
+    'project' => [
+        'description' => 'lorem.......',
+        'project1' => 'project1',
+        'text1' => 'text1',
+        'project2' => 'project2',
+        'text2' => 'text2',
+        'project3' => 'project3',
+        'text3' => 'text3',
+        'project4' => 'project4',
+        'text4' => 'text4',
+        'project5' => 'project5',
+        'text5' => 'text5'
+    ],
+    'skills' => [
+        [
+            'skills' => 'Хороший человек',
+            'procent' => 100
+        ],
+        [
+            'skills' => 'готовка',
+            'procent' => 50
+        ],
+        [
+            'skills' => 'Трудолюбие',
+            'procent' => 40
+        ],
+        [
+            'skills' => 'Целеустремленность',
+            'procent' => 90
+        ],
+        [
+            'skills' => 'Знание иностранных языков',
+            'procent' => 10
+        ]
+    ]
+
+];
